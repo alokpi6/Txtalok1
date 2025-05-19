e150db39cd6eb21456bb4b73d92c767daae18da6
alokpi6 <siliconesafari@gmail.com>
2025-05-19 21:05:33 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,11 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "20288951"))
+API_HASH = environ.get("API_HASH", "e8cb5fb7a475b5f5eb3b0ef0e6ca03a8")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+OWNER = int(environ.get("OWNER", "7833842279"))
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
